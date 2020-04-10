760f2cc2a5303bf74792866c5ddd919c9dcf64d9
piteredo <piteredo@hotmail.com>
2020-04-10 23:31:30 +0900

    init

---
+++ C++/helloworld2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string str = "Hello";
+    string str2;
+    cout << str + "?..." << endl;
+    cin >> str2;
+    std::cout << str + " " + str2 + "!" << std::endl;
+    return 0;
+}
